f94776efa5ca8f6e538215f127621005e3c8c14f
Mohammed B kemal <mickymicky718@gmail.com>
2026-03-02 03:47:04 +0400

    Add CyberTrainerScreen with placeholder text

---
+++ citizen-app/lib/screens/cyber_trainer.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CyberTrainerScreen extends StatelessWidget {
+  const CyberTrainerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Cyber Trainer – Coming Soon'),
+    );
+  }
+}
