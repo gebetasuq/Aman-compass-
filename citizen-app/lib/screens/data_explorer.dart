e67c63140bd1723312f45271e3c74fe8395f6124
Mohammed B kemal <mickymicky718@gmail.com>
2026-03-02 03:50:50 +0400

    Add DataExplorerScreen with placeholder text

---
+++ citizen-app/lib/screens/data_explorer.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DataExplorerScreen extends StatelessWidget {
+  const DataExplorerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Data Explorer – Coming Soon'),
+    );
+  }
+}
