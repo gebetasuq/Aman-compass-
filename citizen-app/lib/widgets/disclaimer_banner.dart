19b80a24a96c5e73e046fa4d1c6021e0439445b8
Mohammed B kemal <mickymicky718@gmail.com>
2026-03-02 03:19:22 +0400

    Add DisclaimerBanner widget for displaying disclaimers
    Implemented a new widget to display disclaimers in a banner format.

---
+++ citizen-app/lib/widgets/disclaimer_banner.dart
@@ -0,0 +1,23 @@
+```dart
+import 'package:flutter/material.dart';
+import 'generated/l10n.dart';
+
+class DisclaimerBanner extends StatelessWidget {
+  const DisclaimerBanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final loc = AppLocalizations.of(context)!;
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(8),
+      color: Colors.red[900],
+      child: Text(
+        loc.disclaimer,
+        style: const TextStyle(color: Colors.white, fontSize: 12),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
+
