962a5042c556219c1fbe4d24a380866bd8051b48
Mohammed B kemal <mickymicky718@gmail.com>
2026-03-02 04:23:10 +0400

    Create constants.js

---
+++ gov-dashboard/utils/constants.js
@@ -0,0 +1,12 @@
+export const EMIRATES = [
+  'all',
+  'Abu Dhabi',
+  'Dubai',
+  'Sharjah',
+  'Ajman',
+  'Umm Al Quwain',
+  'Ras Al Khaimah',
+  'Fujairah',
+];
+
+export const DASHBOARD_TABS = ['Overview', 'Heatmaps', 'Phishing', 'Predictive'];
