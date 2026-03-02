9bbac831257d6be9fefb5046c25ffa405d108a36
Mohammed B kemal <mickymicky718@gmail.com>
2026-03-02 04:13:53 +0400

    Add Header component with title prop

---
+++ gov-dashboard/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header({ title }) {
+  return (
+    <header style={{ marginBottom: '20px' }}>
+      <h1>{title}</h1>
+    </header>
+  );
+}
+```
